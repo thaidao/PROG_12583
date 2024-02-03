426349af4b67714f79b2539570b6085605f96ccd
Thai Dao (Thomas) <daonamthai@gmail.com>
2024-02-03 01:04:14 -0500

    replace test code by real input value

--- 2_Assignment/Thai_991739094_Assignment_1st.py
+++ 2_Assignment/Thai_991739094_Assignment_1st.py
@@ -36,11 +36,11 @@ fItemUnitPrice_4 = 0.99
 fItemUnitPrice_5 = 0.99
 
 # Total price
-fItemUnitTotalPrice_1 = 0
-fItemUnitTotalPrice_2 = 0
-fItemUnitTotalPrice_3 = 0
-fItemUnitTotalPrice_4 = 0
-fItemUnitTotalPrice_5 = 0
+fItemTotalPrice_1 = 0
+fItemTotalPrice_2 = 0
+fItemTotalPrice_3 = 0
+fItemTotalPrice_4 = 0
+fItemTotalPrice_5 = 0
 
 # Discount Amount
 fDiscountAmount = 0
@@ -72,39 +72,39 @@ print('Hello and welcome to the online fruit stand.\n'\
 print('First we have %s for $%0.2f'% (ITEM1,fItemUnitPrice_1))
 print("How many would you like?")
 iItemQty_1 = int(input())
-fItemUnitTotalPrice_1 = iItemQty_1*fItemUnitPrice_1
-#print(fItemUnitTotalPrice_1)
+fItemTotalPrice_1 = iItemQty_1*fItemUnitPrice_1
+#print(fItemTotalPrice_1)
 
 # print('First we have %s for $%0.2f'% (ITEM2,fItemUnitPrice_2))
 # print("How many would you like?")
 # iItemQty_2 = int(input())
-# fItemUnitTotalPrice_2 = iItemQty_2*fItemUnitPrice_2
-# #print(fItemUnitTotalPrice_2)
+# fItemTotalPrice_2 = iItemQty_2*fItemUnitPrice_2
+# #print(fItemTotalPrice_2)
 
 # print('First we have %s for $%0.2f'% (ITEM3,fItemUnitPrice_3))
 # print("How many would you like?")
 # iItemQty_3 = int(input())
-# fItemUnitTotalPrice_3 = iItemQty_3*fItemUnitPrice_3
-# #print(fItemUnitTotalPrice_3)
+# fItemTotalPrice_3 = iItemQty_3*fItemUnitPrice_3
+# #print(fItemTotalPrice_3)
 
 # print('First we have %s for $%0.2f'% (ITEM4,fItemUnitPrice_4))
 # print("How many would you like?")
 # iItemQty_4 = int(input())
-# fItemUnitTotalPrice_4 = iItemQty_4*fItemUnitPrice_4
-# #print(fItemUnitTotalPrice_4)
+# fItemTotalPrice_4 = iItemQty_4*fItemUnitPrice_4
+# #print(fItemTotalPrice_4)
 
 # print('First we have %s for $%0.2f'% (ITEM5,fItemUnitPrice_5))
 # print("How many would you like?")
 # iItemQty_5 = int(input())
-# fItemUnitTotalPrice_5 = iItemQty_5*fItemUnitPrice_5
-# #print(fItemUnitTotalPrice_5)
+# fItemTotalPrice_5 = iItemQty_5*fItemUnitPrice_5
+# #print(fItemTotalPrice_5)
 
 
 #==================REQUIRMENT_3================================================
 # 3. Ask them for the amount of the discount they get (from 0% to 100%). 
 # We'll just assume they will be honest about this :-)
 print("What is your discount? (0 - 100 percent)")
-fDiscountAmount = float(input())
+fDiscountPercentage = float(input())
 
 
 #==================REQUIRMENT_4================================================
@@ -147,21 +147,66 @@ prtStr='|{0:>35} '.format(str1) + '|{0:^13}'.format(str2)+\
 print(prtStr)
 
 # ITEM1
-str1,str2,str3,str4 = "Apples","23","0.50","11.50"
+str1,str2,str3,str4 = ITEM1,iItemQty_1,fItemUnitPrice_1,fItemTotalPrice_1
 prtStr='|{0:>35} '.format(str1) + '|{0:^13}'.format(str2)+\
-		'|{0:>13}'.format(str3) + '|{0:>13}|'.format(str4)
+		'|{0:>12} '.format(str3) + '|{0:>12} |'.format(str4)
 print(prtStr)
 
 # ITEM2
-str1,str2,str3,str4 = "Oranges","47","0.65","30.55"
+str1,str2,str3,str4 = ITEM2,iItemQty_2,fItemUnitPrice_2,fItemTotalPrice_2
 prtStr='|{0:>35} '.format(str1) + '|{0:^13}'.format(str2)+\
-		'|{0:>13}'.format(str3) + '|{0:>13}|'.format(str4)
+		'|{0:>12} '.format(str3) + '|{0:>12} |'.format(str4)
 print(prtStr)
+
+# ITEM3
+str1,str2,str3,str4 = ITEM3,iItemQty_3,fItemUnitPrice_3,fItemTotalPrice_3
+prtStr='|{0:>35} '.format(str1) + '|{0:^13}'.format(str2)+\
+		'|{0:>12} '.format(str3) + '|{0:>12} |'.format(str4)
+print(prtStr)
+
+# ITEM4
+str1,str2,str3,str4 = ITEM4,iItemQty_4,fItemUnitPrice_4,fItemTotalPrice_4
+prtStr='|{0:>35} '.format(str1) + '|{0:^13}'.format(str2)+\
+		'|{0:>12} '.format(str3) + '|{0:>12} |'.format(str4)
+print(prtStr)
+
+# ITEM5
+str1,str2,str3,str4 = ITEM5,iItemQty_5,fItemUnitPrice_5,fItemTotalPrice_5
+prtStr='|{0:>35} '.format(str1) + '|{0:^13}'.format(str2)+\
+		'|{0:>12} '.format(str3) + '|{0:>12} |'.format(str4)
+print(prtStr)
+
+# Calculate Subtotal1
+fSubTotal_1 = fItemTotalPrice_1+fItemTotalPrice_2+fItemTotalPrice_3+\
+	fItemTotalPrice_4+fItemTotalPrice_5
+
+# Calculate Subtotal2
+fSubTotal_2 = fSubTotal_1 + fHST
+
+# Calculate discount amount
+fDiscountAmount = fDiscountPercentage*fSubTotal_2/100
 
 print("|----------------------------------------------------------------|-------------|")
 # TOTAL
-str1,str2 = "Sub total 1","184.71"
-prtStr='|{0:>63} '.format(str1) + '|{0:>13}|'.format(str2)
+str1,str2 = "Sub total 1",fSubTotal_1
+prtStr='|{0:>63} '.format(str1) + '|{0:>12} |'.format(str2)
+print(prtStr)
+
+str1,str2 = "H.S.T",fHST
+prtStr='|{0:>63} '.format(str1) + '|{0:>12} |'.format(str2)
+print(prtStr)
+
+str1,str2 = "Sub total 2",fSubTotal_2
+prtStr='|{0:>63} '.format(str1) + '|{0:>12} |'.format(str2)
+print(prtStr)
+
+str1 = "Discount (" + str(fDiscountPercentage) + "%)"
+str2 = fDiscountAmount
+prtStr='|{0:>63} '.format(str1) + '|{0:>12} |'.format(str2)
+print(prtStr)
+
+str1,str2 = "Amount Due", fSubTotal_2-fDiscountAmount
+prtStr='|{0:>63} '.format(str1) + '|{0:>12} |'.format(str2)
 print(prtStr)
 
 print("|==============================================================================|")
